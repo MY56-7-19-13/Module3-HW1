7cdcfcbff04244f9350f85ea71ba8e6c20aea7e1
Maksym Yankovskyi <maks.yankovskiy27@gmail.com>
2023-05-01 11:40:34 +0300

    update homework

--- Homework1/MyCollection.cs
+++ Homework1/MyCollection.cs
@@ -7,50 +7,87 @@ using System.Threading.Tasks;
 
 namespace Homework1
 {
-    public class MyCollection : IEnumerable, IEnumerator
+    public class MyCollection<T> : IEnumerable<T>
     {
-        private List<string> _books;
-
-        private int _position = -1;
-        public MyCollection(List<string> books)
+        private T[] _data;
+        private int _count;
+        public MyCollection()
         {
-            _books = books;
+            _data = Array.Empty<T>();
+            _count = 0;
         }
-
-        public void Dispose()
+        public void Add(T item)
         {
-        }
-
-        public bool MoveNext()
-        {
-            if (_position < _books.Count - 1)
+            var newData = new T[_data.Length + 1];
+            for (int i = 0; i < _data.Length; i++)
             {
-                _position++;
-                return true;
+                newData[i] = _data[i];
             }
-            else
+            newData[_count] = item;
+            _data = newData;
+            _count++;
+        }
+        public void AddRange(IEnumerable<T> values)
+        {
+            foreach (var item in values)
             {
-                Reset();
-                return false;
+                Add(item);
+            }
+
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            for (int i = 0; i < _count; i++)
+            {
+                yield return _data[i];
             }
         }
 
-        public void Reset()
+        public void Remove(T value)
         {
-            _position = -1;
-        }
-
-        public object Current
-        {
-            get
+            int index = 0;
+            for (int i = 0; i < _count; i++)
             {
-                return _books[_position];
+                if (_data[i].Equals(value))
+                {
+                    index = i;
+                }
             }
+            for (int i = index; i < _count; i++)
+            {
+                if (i + 1 < _count)
+                {
+                    _data[i] = _data[i + 1];
+                }
+                else
+                {
+                    _data[i] = default(T);
+                }
+            }
+            _count--;
+        }
+        public void RemoveAt(int index)
+        {
+            if (index < -1 && index >= _count)
+            {
+                return;
+            }
+            for (int i = index; i < _count - 1; i++)
+            {
+                _data[i] = _data[i + 1];
+            }
+            _count--;
+
+        }
+        public void Sort()
+        {
+            Array.Sort(_data, 0, _count);
         }
 
         IEnumerator IEnumerable.GetEnumerator()
         {
-            return this as IEnumerator;
+            return GetEnumerator();
         }
     }
 }
---
+++ Homework1/User.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework1
+{
+    public class User : IComparable<User>
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+
+        public int CompareTo(User? other)
+        {
+            return Age.CompareTo(other.Age);
+        }
+
+        public override string ToString()
+        {
+            return $"Name: {Name} | Age: {Age}";
+        }
+    }
+}
